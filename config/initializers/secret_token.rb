78493c34a008f138911d2451f56cb096d3cd4820
aoslm <aoslm12193@gmail.com>
2013-03-15 14:21:27 +0100

    New

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+First::Application.config.secret_token = '9c35eabcf481fce05a7da7782cac4a708f4dfe34fecc4ee9f294a53935448330692ad683dbae026cbac37445915b142c2c14b3c9e177ae3dff36b1878b385ef2'
